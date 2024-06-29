2b52274198fb55a9cd4f26a7004d26035e941b60
arithex <arithex@users.noreply.github.com>
2024-06-28 21:56:00 -0700

    Version v2.4.1.13

--- Falcon BMS Alternative Launcher/Properties/AssemblyInfo.cs
+++ Falcon BMS Alternative Launcher/Properties/AssemblyInfo.cs
@@ -52,6 +52,6 @@ using System.Windows;
 [assembly: AssemblyVersion("2.4.1.9999")]
 [assembly: AssemblyFileVersion("2.4.1.9999")]
 #else
-[assembly: AssemblyVersion("2.4.1.12")]
-[assembly: AssemblyFileVersion("2.4.1.12")]
+[assembly: AssemblyVersion("2.4.1.13")]
+[assembly: AssemblyFileVersion("2.4.1.13")]
 #endif
