f790c27748fe016b5c9677023b46524d343a2dee
arithex <arithex@users.noreply.github.com>
2024-07-11 13:15:16 -0700

    Improve diagnostic logging for DirectInput devices.

--- Falcon BMS Alternative Launcher/Input/DeviceControl.cs
+++ Falcon BMS Alternative Launcher/Input/DeviceControl.cs
@@ -48,7 +48,12 @@ namespace FalconBMS.Launcher.Input
             {
                 if (suppressList.IsDeviceSuppressed(dev.InstanceGuid) ||
                     suppressList.IsDeviceSuppressed(dev.ProductGuid))
+                {
+                    Diagnostics.Log($"Ignoring suppressed device: pidvid {dev.ProductGuid} instance {dev.InstanceGuid}", Diagnostics.LogLevels.Info);
                     continue;
+                }
+
+                Diagnostics.Log($"Found device: pidvid {dev.ProductGuid} instance {dev.InstanceGuid}", Diagnostics.LogLevels.Info);
 
                 Device hwdev = new Device(dev.InstanceGuid);
                 JoyAssgn joy = new JoyAssgn(hwdev);
