7e4f67ffbfd1b885009ed625845816e99bdf2102
arithex <arithex@users.noreply.github.com>
2024-09-07 16:24:23 -0700

    Version 2.4.1.16

--- Falcon BMS Alternative Launcher/Properties/AssemblyInfo.cs
+++ Falcon BMS Alternative Launcher/Properties/AssemblyInfo.cs
@@ -49,9 +49,9 @@ using System.Windows;
 // すべての値を指定するか、次を使用してビルド番号とリビジョン番号を既定に設定できます
 // 既定値にすることができます:
 #if DEBUG
-[assembly: AssemblyVersion("2.4.1.9999")]
+[assembly:     AssemblyVersion("2.4.1.9999")]
 [assembly: AssemblyFileVersion("2.4.1.9999")]
 #else
-[assembly: AssemblyVersion("2.4.1.14")]
-[assembly: AssemblyFileVersion("2.4.1.15")]
+[assembly:     AssemblyVersion("2.4.1.16")]
+[assembly: AssemblyFileVersion("2.4.1.16")]
 #endif
