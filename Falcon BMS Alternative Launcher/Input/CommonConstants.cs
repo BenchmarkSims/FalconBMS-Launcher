46db48db3a7522c20c24c63418f026f15b7c1106
chihirobelmo <chihirobelmo@gmail.com>
2025-03-21 00:56:16 +0900

    fix value

--- Falcon BMS Alternative Launcher/Input/CommonConstants.cs
+++ Falcon BMS Alternative Launcher/Input/CommonConstants.cs
@@ -29,7 +29,7 @@ namespace FalconBMS.Launcher.Input
 
         public static readonly int AXISMIN = 0;
         public static readonly int AXISMAX = 65536;
-        public static readonly float AXIS_NEUTRAL_TOLERANCE = 0.1f;
+        public static readonly int AXIS_NEUTRAL_TOLERANCE = AXISMAX / 128;
 
         public static readonly int FLUSHTIME1 = 900;
         public static readonly int FLUSHTIME2 = 1500;
